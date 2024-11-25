914ad21f0cccf1690118718fb1fe6f56005c1a77
Carolina Ayala <133041998+carol230@users.noreply.github.com>
2024-11-24 21:11:26 -0500

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from scipy import optimize
-def f(v0, vf, n, a):
-    return lambda x: vf - v0*(1+x)**n - a*(((1+x)**n-(1+x))/x)
-def obtener_interes():
-    return optimize.newton(f(100, 105.31, 2, 5), 1e-5)
-
-print(obtener_interes())
